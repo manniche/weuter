2aed7002e0c79c44ed6f891e909201b8c5b4b43e
Steen Manniche <steen@manniche.net>
2013-03-26 13:13:08 +0000

    adding rails default site

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Weuter::Application.config.secret_token = 'fdbdc42def2bb215e30ce39e382dec3e1de67b06509665289f007bca9e1be4ac0356cd43ddf5a1e7bb6c7bd33479d17f748ae45bff95608ec6d0e4584194f5d1'
